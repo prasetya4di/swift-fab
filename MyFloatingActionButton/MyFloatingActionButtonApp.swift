c4783ad41d92199453fac198d274343146b3cc11
praset <prasetyaarfanto@gmail.com>
2022-03-07 11:11:48 +0700

    Initial Commit

---
+++ MyFloatingActionButton/MyFloatingActionButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFloatingActionButtonApp.swift
+//  MyFloatingActionButton
+//
+//  Created by Pras Adi on 07/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFloatingActionButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
