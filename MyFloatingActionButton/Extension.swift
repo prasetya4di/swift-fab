ae10012313d435205ec549640f567cee26bba0c9
praset <prasetyaarfanto@gmail.com>
2022-03-08 14:10:07 +0700

    add extra parameter for more customizable

--- MyFloatingActionButton/Extension.swift
+++ MyFloatingActionButton/Extension.swift
@@ -12,11 +12,17 @@ extension View {
     backgroundColor: Color,
     content: fabView,
     position: FabPosition = .trailing,
+    shadowColor: Color = .gray,
+    cornerRadius: CGFloat = 24,
+    elevation: CGFloat = 1,
     action: @escaping () -> Void
   ) -> some View {
     self.modifier(
       FloatingActionButton(
         backgroundColor: backgroundColor,
+        shadowColor: shadowColor,
+        elevation: elevation,
+        cornerRadius: cornerRadius,
         view: content,
         action: action,
         position: position
--- MyFloatingActionButton/FloatingActionButton.swift
+++ MyFloatingActionButton/FloatingActionButton.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct FloatingActionButton<fabView: View>: ViewModifier {
   let backgroundColor: Color
+  var shadowColor: Color = .gray
+  var elevation: CGFloat = 1
+  var cornerRadius: CGFloat = 24
   let view: fabView
   let action: () -> Void
   var position: FabPosition = .trailing
   
-  private let margin: CGFloat = 24
-  @State private var rect: CGRect = CGRect()
+  private let margin: CGFloat = 20
+  @State private var viewRect: CGRect = CGRect()
   
   func body(content: Content) -> some View {
     GeometryReader { geo in
@@ -27,14 +30,14 @@ struct FloatingActionButton<fabView: View>: ViewModifier {
           view
         }
         .buttonStyle(PlainButtonStyle())
-        .background(backgroundColor.cornerRadius(24))
+        .background(backgroundColor.cornerRadius(cornerRadius))
         .padding(16)
-        .shadow(color: .gray, radius: 3, x: 0, y: 1)
+        .shadow(color: shadowColor, radius: 3, x: 0, y: elevation)
         .background(
-          GeometryGetter(rect: $rect)
+          GeometryGetter(rect: $viewRect)
         )
         .position(x: getXPosition(geo),
-                  y: geo.size.height - margin)
+                  y: geo.size.height - viewRect.height / 3 - margin)
       }
     }
   }
@@ -42,11 +45,11 @@ struct FloatingActionButton<fabView: View>: ViewModifier {
   func getXPosition(_ geo: GeometryProxy) -> CGFloat {
     switch position {
     case .leading:
-      return 0 + rect.width / 2
+      return 0 + viewRect.width / 3 + margin
     case .center:
       return geo.size.width / 2
     case .trailing:
-      return geo.size.width - rect.width / 2
+      return geo.size.width - viewRect.width / 3 - margin
     }
   }
 }
